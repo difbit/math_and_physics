a4069f6029ca159e4cf9705dfd9e293909e20297
difbit <timo.kekalainen@hotmail.com>
2019-12-20 23:45:48 +0200

    Added a timer and class method now returns a generator object

--- simple_prime.py
+++ simple_prime.py
@@ -1,29 +1,39 @@
 
+import time
+
+
 class Prime(object):
 
     def __init__(self, limit):
         self.limit = limit
 
-    def generate_primes(self):
-        primes = []
+    def timeit(f):
+        def timed(*args, **kw):
+            before = time.time()
+            result = f(*args, **kw)
+            after = time.time()
+
+            print "Elapsed time: ", after - before
+            return result
+        return timed
+
+    @timeit
+    def get_primes(self):
         range_limit = self.limit
 
         for num in range(2, range_limit):
             i = 2
             if num == 2:
-                primes.append(num) 
+                yield num
             while i < num:
                 if i <= (num / 2):
                     if num % i == 0:
                         break
                 else:
-                    primes.append(num) 
+                    yield num
                     break
                 i += 1
 
-        return primes
-
-# This is just a convention
 if __name__ == '__main__':
-    prime = Prime(100)
-    print prime.generate_primes()
+    prime = Prime(10000000)
+    print prime.get_primes()
